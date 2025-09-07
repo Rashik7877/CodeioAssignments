9c2bbc9096c499e73ed04a2eacf3a1a3a51f8f35
rashik7877 <mohammed786rashik@gmail.com>
2025-09-07 21:22:52 +0530

    problem 1 completed

---
+++ src/TheLightBulbTest.java
@@ -0,0 +1,11 @@
+public class TheLightBulbTest {
+    public static void main(String[] args) {
+        int n = 26;
+        if ( n % 2 == 0){
+            System.out.println("ON");
+        }
+        else{
+            System.out.println("OFF");
+        }
+    }
+}
