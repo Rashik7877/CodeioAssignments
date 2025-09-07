d4d5893b1ace398d7e9c60312e5a889f55ecafc6
rashik7877 <mohammed786rashik@gmail.com>
2025-09-07 21:14:06 +0530

    problem 3 completed

---
+++ src/TheFasterRunner.java
@@ -0,0 +1,15 @@
+public class TheFasterRunner {
+    public static void main(String[] args) {
+        int time1 = 13;
+        int time2 = 12;
+        if (time1 > time2){
+            System.out.println("Arjun is faster");
+        }
+        else if (time2 > time1) {
+            System.out.println("Varun is faster");
+        }
+        else{
+            System.out.println("tie");
+        }
+    }
+}
