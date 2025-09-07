e831bd016d1774f351dcca681880f71562945256
rashik7877 <mohammed786rashik@gmail.com>
2025-09-07 21:19:14 +0530

    problem 1 completed

---
+++ src/TheBankBalance.java
@@ -0,0 +1,14 @@
+public class TheBankBalance {
+    public static void main(String[] args) {
+        int balance = 0;
+        if (balance > 0){
+            System.out.println("Positive Balance");
+        }
+        else if (balance < 0) {
+            System.out.println("Negative Balance");
+        }
+        else {
+            System.out.println("Zero Balance");
+        }
+    }
+}
