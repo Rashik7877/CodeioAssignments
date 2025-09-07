6b9a6b2949cd0ade90898273b7ab61f087ee431b
rashik7877 <mohammed786rashik@gmail.com>
2025-09-07 21:03:33 +0530

    problem 5 completed

---
+++ src/TheMagicCalender.java
@@ -0,0 +1,11 @@
+public class TheMagicCalender {
+    public static void main(String[] args) {
+        int year = 2024;
+        if (year % 4 == 0){
+            System.out.println("Its is leap year");
+        }
+        else{
+            System.out.println("Its not a leap year");
+        }
+    }
+}
