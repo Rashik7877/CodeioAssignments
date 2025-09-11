999335043d7cf1acd4adade6907222d064d689bc
rashik7877 <mohammed786rashik@gmail.com>
2025-09-11 22:34:36 +0530

    assignment set 2 1st answe

---
+++ Assignment1set2/SpeedLImitCheck.java
@@ -0,0 +1,11 @@
+public class SpeedLImitCheck {
+    public static void main(String[] args) {
+        int speed = 60;
+        if (speed>60){
+            System.out.println("Overspeeding! Pay Fine");
+        }
+        else {
+            System.out.println("You are Within the Speed limit");
+        }
+    }
+}
